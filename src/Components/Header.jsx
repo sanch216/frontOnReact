655e8b22af86a6410e4e866b3b008ca5c67b11f7
sanch216 <checking99@mail.ru>
2025-11-15 17:49:26 +0600

    init com

---
+++ src/Components/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.css"
+export default function Header() {
+    return (
+        <div className="divArea-1">
+
+            <div>
+                <h1>DoDel</h1>
+                <p1>*Image*</p1>
+            </div>
+
+            <div>
+                <button>
+                    Вход
+                </button>
+                <button>
+                    Регистрация
+                </button>
+            </div>
+
+        </div>
+    )
+}
